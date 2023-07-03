c2d12cb9de2687153013050f5549d865f282b129
rickmad11 <118388835+rickmad11@users.noreply.github.com>
2023-07-03 18:55:04 +0200

    Update

--- utility_help_functions.hpp
+++ utility_help_functions.hpp
@@ -29,24 +29,24 @@ int Release_Mode_Warning = []() -> int { std::cerr << "Error: Release mode enabl
 
 #else
 // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/crtsetreportfile?view=msvc-170 ONLY DEBUG MODE
-void MEMORY_LEAK_DUMP()
+static void MEMORY_LEAK_DUMP()
 {
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
 	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
 	_CrtDumpMemoryLeaks();
 }
 // ONLY DEBUG MODE this Function is for if statements
-int MEMORY_LEAK_INT()
+static int MEMORY_LEAK_INT()
 {
 	return _CrtDumpMemoryLeaks();
 }
 // ONLY DEBUG MODE this Function is only used to satisfy std::boolalpha 
-bool MEMORY_LEAK_BOOL()
+static bool MEMORY_LEAK_BOOL()
 {
 	return static_cast<bool>(_CrtDumpMemoryLeaks());
 }
 // Breakpoint on the Location where the memory leak occurred
-void LEAK_WHERE(int LOCATION_ID)
+static void LEAK_WHERE(int LOCATION_ID)
 {
 	_CrtSetBreakAlloc(LOCATION_ID);
 }
